c40d808df9b14e3897d3e441d153b2e319d4e10a
Joon <daniel.joon.yi@gmail.com>
2021-07-13 18:19:52 -0700

    Hide nav on click / adjust font sizing

--- js/script.js
+++ js/script.js
@@ -1,6 +1,14 @@
 const hamburger = document.getElementById('hamburger');
 const nav = document.getElementById('nav');
+const navLink = document.querySelectorAll('.nav-link');
 
 hamburger.addEventListener('click', () => {
   nav.classList.toggle('show');
+});
+
+navLink.forEach((link) => {
+  link.addEventListener('click', () => {
+    nav.classList.remove('show');
+  })
 })
+
